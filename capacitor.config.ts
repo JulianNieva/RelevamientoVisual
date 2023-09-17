95d6afbf6da6f5bf2b85d79aaaea02bb78c4ac73
JulianNieva <juliannieva90@gmail.com>
2023-09-17 19:30:42 -0300

    Agregado proyecto Relevamiento Visual con login funcional

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'relevamiento visual',
+  appId: 'com.nieva.relevamientovisual',
+  appName: 'Relevamiento Visual',
   webDir: 'www',
   server: {
     androidScheme: 'https'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'relevamiento-visual-b23f6',
+    appId: '1:520174542285:web:64438555a5f87932dbb884',
+    storageBucket: 'relevamiento-visual-b23f6.appspot.com',
+    apiKey: 'AIzaSyATGeaYCsxO42yLnPpyH3WGx6qyins7OEk',
+    authDomain: 'relevamiento-visual-b23f6.firebaseapp.com',
+    messagingSenderId: '520174542285',
+  },
   production: false
 };
 
